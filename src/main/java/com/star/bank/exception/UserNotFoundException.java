3ac863411233f6531e41238fd294fbce92e75ea4
Belousoveu <belousov.eu@gmail.com>
2025-02-09 20:44:00 +0300

    Created controller with endpoint for get userId and response User's recommendations. Created test methods for controller class.

---
+++ src/main/java/com/star/bank/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.star.bank.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String id) {
+
+        super(String.format("User with id %s not found", id));
+    }
+}
