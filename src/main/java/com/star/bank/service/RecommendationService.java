957354b37fa80521cf27b92aeb6a4beb3dd716ec
denis <denistigrov@icloud.com>
2025-02-09 15:24:05 +0300

    create sql query

--- src/main/java/com/star/bank/service/RecommendationService.java
+++ src/main/java/com/star/bank/service/RecommendationService.java
@@ -1,26 +1,24 @@
 package com.star.bank.service;
 
-import com.star.bank.model.Recommendation;
-import com.star.bank.repository.RecommendationServiceRepository;
+import com.star.bank.repository.TransactionRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class RecommendationService implements RecommendationServiceRepository {
+public class RecommendationService implements TransactionRepository {
+    private  final TransactionRepository transactionRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-
-    private  final RecommendationServiceRepository repository;
-
-    public RecommendationService(RecommendationServiceRepository repository) {
-        this.repository = repository;
+    public RecommendationService(JdbcTemplate jdbcTemplate, TransactionRepository transactionRepository, JdbcTemplate jdbcTemplate1) {
+        this.transactionRepository = transactionRepository;
+        this.jdbcTemplate = jdbcTemplate1;
     }
 
     @Override
-    public List<Recommendation> findAllRecommendations(Long id) {
-        // код с отработкой ошибок
-
-        return repository.findAllRecommendations(id);
+    public List<String> getRecommendation(String userId) {
+        String result = "SELECT id FROM recommendations WHERE user_id = ?";
+        return jdbcTemplate.queryForList(result, String.class, userId);
     }
-
 }
