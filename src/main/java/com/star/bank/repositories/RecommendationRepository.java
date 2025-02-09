2c6d2254be058025c878b354b6cf0810bd9f5640
Unique-Kris <titina91@mail.ru>
2025-02-09 22:55:32 +0300

    New commit for review

--- src/main/java/com/star/bank/repositories/RecommendationRepository.java
+++ src/main/java/com/star/bank/repositories/RecommendationRepository.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RecommendationRepository {
 
-    public RecommendationRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
-    }
+    public RecommendationRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {}
 
-    public boolean checkProductRules(String userId) {
-        return true;
+    public boolean checkProductRules(String userId, Product product) {
+        String query = product.getQuery(userId);
+        Integer result = jdbcTemplate.queryForObject(query, Integer.class);
+        return result != null && result > 0;
     }
 }
