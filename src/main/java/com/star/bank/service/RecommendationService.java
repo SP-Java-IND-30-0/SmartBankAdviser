0ae81c8a2208e1ed0372b238f1665e6545d94b3b
denis <denistigrov@icloud.com>
2025-02-10 09:56:11 +0300

    service class fix

--- src/main/java/com/star/bank/service/RecommendationService.java
+++ src/main/java/com/star/bank/service/RecommendationService.java
@@ -1,35 +1,21 @@
 package com.star.bank.service;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.logging.Logger;
 
 @Service
 public class RecommendationService implements TransactionRepository {
-    private Logger logger = LoggerFactory.getLogger(TransactionRepository.class);
-    private final Rule rule;
-    private final Product product;
     private  final TransactionRepository transactionRepository;
-    private final JdbcTemplate jdbcTemplate;
+    private final List<Product> productList;
 
-    private String productQuery = product.getQuery();
-    private String ruleQuery = rule.getSubQuery;
-
-    public RecommendationService(JdbcTemplate jdbcTemplate, TransactionRepository transactionRepository) {
+    public RecommendationService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
-        this.jdbcTemplate = jdbcTemplate;
     }
 
-//    public List<Recommendation> insertRecommendation(String productQuery, String ruleQuery) {
-//        return recommendation = jdbcTemplate.query(productQuery, ruleQuery);
-//    }
+    public void sendRecommendation(String userId) {
 
-    public List<Recommendation> getRecommendation(String userId) {
-        String result = "SELECT id FROM recommendations WHERE user_id = ?";
-        return jdbcTemplate.queryForList(result, Recommendation.class, userId);
+
+
     }
 
 }
