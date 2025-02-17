04c95d7bbd0cdd927be8b93a5c34d5069208a92b
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-02-17 23:45:26 +0300

    Исправил согласно замечаниям: 1.Метод в контроллере теперь называется saveDynamicRule, как указано в задании, что соответствет методу сервиса saveDynamicRule (убрал addDynamicRule). 2.Возвращается тот же объект, который пришел в запросе + код ответа 201 Created, что корректно для сохранения. 3.Убрал ручной вывод логов; 4.getDynamicRules() оставил возвращаемый код 200;

--- src/main/java/com/star/bank/controller/DynamicRuleController.java
+++ src/main/java/com/star/bank/controller/DynamicRuleController.java
@@ -3,7 +3,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/rule")
@@ -14,22 +13,19 @@ public class DynamicRuleController {
     private final DynamicRuleService dynamicRuleService;
 
     @PostMapping
-    public ResponseEntity<DynamicRuleDto> addDynamicRule(@RequestBody DynamicRuleDto dynamicRuleDto) {
-        log.info("Добавление нового динамического правила: {}", dynamicRuleDto);
-        DynamicRuleDto createdRule = dynamicRuleService.addDynamicRule(dynamicRuleDto);
-        return ResponseEntity.ok(createdRule);
+    public ResponseEntity<DynamicRuleDto> saveDynamicRule(@RequestBody DynamicRuleDto dynamicRuleDto) {
+        dynamicRuleService.saveDynamicRule(dynamicRuleDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dynamicRuleDto); // 201 Created + возврат переданного объекта
     }
 
     @GetMapping
-    public ResponseEntity<List<DynamicRuleDto>> getDynamicRules() {
-        log.info("Получение списка всех динамических правил");
+    public ResponseEntity<List<DynamicRuleDto>> getDynamicRules() {;
         List<DynamicRuleDto> rules = dynamicRuleService.getDynamicRules();
         return ResponseEntity.ok(rules);
     }
 
     @DeleteMapping("/{ruleId}")
     public ResponseEntity<Void> deleteDynamicRule(@PathVariable String ruleId) {
-        log.info("Удаление динамического правила с ID: {}", ruleId);
         dynamicRuleService.deleteDynamicRule(ruleId);
         return ResponseEntity.noContent().build();
     }
