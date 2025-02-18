0400be2fa779b450d4ae3e9f36127e146feaac1d
denis <denistigrov@icloud.com>
2025-02-19 00:04:20 +0300

    create DynamicRuleRepository and create get method in RecommendationService

--- src/main/java/com/star/bank/service/RecommendationService.java
+++ src/main/java/com/star/bank/service/RecommendationService.java
@@ -1,20 +1,25 @@
 package com.star.bank.service;
 
-
 import com.star.bank.model.dto.PersonalRecommendationDto;
+import com.star.bank.model.product.DynamicRule;
 import com.star.bank.model.product.Product;
+import com.star.bank.repositories.DynamicRuleRepository;
 import com.star.bank.repositories.RecommendationRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class RecommendationService {
     private final RecommendationRepository repository;
+    private final DynamicRuleRepository dynamicRuleRepository;
     private final Set<Product> products;
 
-    public RecommendationService(RecommendationRepository repository, Set<Product> products) {
+
+    public RecommendationService(RecommendationRepository repository, DynamicRuleRepository dynamicRuleRepository, Set<Product> products) {
         this.repository = repository;
+        this.dynamicRuleRepository = dynamicRuleRepository;
         this.products = products;
     }
 
@@ -28,4 +33,17 @@ public class RecommendationService {
         return dto;
     }
 
+    public PersonalRecommendationDto getRecommendation(String userId) {
+        PersonalRecommendationDto dynamicDto = new PersonalRecommendationDto(userId);
+        List<DynamicRule> dynamicRuleList = dynamicRuleRepository.findAll();
+        for (Product product : products) {
+            for (DynamicRule dynamicRule : dynamicRuleList) {
+                if (dynamicRule.getQuery().equals(product.getQuery())) {
+                    repository.checkProductRules(userId, dynamicRule.getQuery());
+                    dynamicDto.addRecommendation(product);
+                }
+            }
+        }
+        return dynamicDto;
+    }
 }
