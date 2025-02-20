a7ba60f25941870312b5e198a39ac20031cbf996
Unique-Kris <titina91@mail.ru>
2025-02-20 22:21:49 +0300

    Updated GlobalControllerAdvice

--- src/main/java/com/star/bank/aspect/GlobalControllerAdvice.java
+++ src/main/java/com/star/bank/aspect/GlobalControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.star.bank.aspect;
 
-import com.star.bank.exception.UserNotFoundException;
+import com.star.bank.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +12,48 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleUserNotFoundException(UserNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleProductNotFoundException(ProductNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(DatabaseAccessException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleDatabaseAccessException(DatabaseAccessException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(DatabaseSaveException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleDatabaseSaveException(DatabaseSaveException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(DuplicateRuleException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDuplicateRuleException(DuplicateRuleException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(InvalidProductIdException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidProductIdException(InvalidProductIdException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(InvalidQueryArgumentsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidQueryArgumentsException(InvalidQueryArgumentsException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(InvalidRuleDataException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidRuleDataException(InvalidRuleDataException ex) {
         return ex.getMessage();
     }
 }
