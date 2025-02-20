a6aa4a6ee8d8b38d7cd00c62fd802e68d047ca3e
Unique-Kris <titina91@mail.ru>
2025-02-20 11:42:50 +0300

    Have been changes details

--- src/main/java/com/star/bank/config/CacheConfig.java
+++ src/main/java/com/star/bank/config/CacheConfig.java
@@ -1,67 +1,68 @@
 package com.star.bank.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.CacheManager;
 import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.jmx.support.RegistrationPolicy;
 
+import javax.management.MXBean;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
+@ManagedResource(objectName = "CacheStats:name=userProductRulesCache")
+@MXBean
 public class CacheConfig {
 
+    private Cache<Object, Object> cache;
+
     @Bean
-    @Primary
-    @Qualifier("caffeineCacheManager")
-    public CaffeineCacheManager caffeineCacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("userProductRulesCache");
         cacheManager.setCaffeine(Caffeine.newBuilder()
                 .maximumSize(10000)
                 .expireAfterWrite(600, TimeUnit.SECONDS)
                 .recordStats());
-        return cacheManager;
-    }
 
-    @Bean
-    @Qualifier("cacheManager")
-    public CacheManager cacheManager(@Qualifier("caffeineCacheManager") CacheManager cacheManager) {
-        return cacheManager;
-    }
-
-    @Bean
-    public CacheStatsMBean cacheStatsMBean(CacheManager cacheManager) {
-        if (!(cacheManager instanceof CaffeineCacheManager caffeineCacheManager)) {
-            throw new IllegalStateException("CacheManager не является CaffeineCacheManager");
-        }
-
-        org.springframework.cache.Cache cacheWrapper = caffeineCacheManager.getCache("userProductRulesCache");
-
+        org.springframework.cache.Cache cacheWrapper = cacheManager.getCache("userProductRulesCache");
         Object nativeCache = cacheWrapper.getNativeCache();
 
-        if (!(nativeCache instanceof Cache<?, ?>)) {
-            throw new IllegalStateException("Неподдерживаемый тип кэша: " + nativeCache.getClass());
-        }
-
         @SuppressWarnings("unchecked")
-        Cache<Object, Object> cache = (Cache<Object, Object>) nativeCache;
+        Cache<Object, Object> caffeineCache = (Cache<Object, Object>) nativeCache;
+        this.cache = caffeineCache;
 
-        return new CacheStatsMBean(cache);
+        return cacheManager;
     }
 
+
     @Bean
-    public MBeanExporter mBeanExporter(CacheStatsMBean cacheStatsMBean) {
+    public MBeanExporter mBeanExporter() {
         MBeanExporter exporter = new MBeanExporter();
-        exporter.setBeans(Collections.singletonMap("com.example:type=CacheStats", cacheStatsMBean));
+        exporter.setBeans(Collections.singletonMap("com.star.bank:type=CacheStats", this));
         exporter.setRegistrationPolicy(RegistrationPolicy.IGNORE_EXISTING);
         return exporter;
+    }
+
+    public long getHitCount() {
+        return cache.stats().hitCount();
+    }
+
+    public long getMissCount() {
+        return cache.stats().missCount();
+    }
+
+    public double getHitRate() {
+        return cache.stats().hitRate();
+    }
+
+    public long getEvictionCount() {
+        return cache.stats().evictionCount();
     }
 }
--- src/test/java/com/star/bank/repositories/RecommendationRepositoryCacheTest.java
+++ src/test/java/com/star/bank/repositories/RecommendationRepositoryCacheTest.java
@@ -1,21 +1,18 @@
 package com.star.bank.repositories;
 
-import com.star.bank.SmartBankAdviserApplication;
-import com.star.bank.config.CacheConfig;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {SmartBankAdviserApplication.class, CacheConfig.class})
+@SpringBootTest
 public class RecommendationRepositoryCacheTest {
 
     @Autowired
@@ -24,59 +21,48 @@ public class RecommendationRepositoryCacheTest {
     @Autowired
     private CacheManager cacheManager;
 
-    private static final String USER_ID = "f37ba8a8-3cd5-4976-9f74-2b21f105da67";
+    private static final String USER_ID_1 = "f37ba8a8-3cd5-4976-9f74-2b21f105da67";
+    private static final String USER_ID_2 = "147f6a0f-3b91-413b-ab99-87f081d60d5a";
     private static final String QUERY = "SELECT COUNT(*) > 0 FROM TRANSACTIONS T " +
             "JOIN PUBLIC.PRODUCTS P ON P.ID = T.PRODUCT_ID WHERE T.USER_ID = ? AND P.TYPE='DEBIT'";
 
-    @BeforeAll
-    public static void setUpDatabase() {
-    }
-
-    @AfterAll
-    public static void cleanUp() {
-    }
-
     @Test
     void testCacheUsage() {
-        Boolean firstResult = recommendationRepository.checkProductRules(USER_ID, QUERY);
-        assertNotNull(firstResult, "Первоначальный результат не должен быть null");
+        Boolean firstResult = recommendationRepository.checkProductRules(USER_ID_1, QUERY);
 
         Cache cache = cacheManager.getCache("userProductRulesCache");
-        Cache.ValueWrapper cachedValue = cache.get(USER_ID + "_" + QUERY);
+        Cache.ValueWrapper cachedValue = cache.get(USER_ID_1 + "_" + QUERY);
 
-        System.out.println("Первый результат: " + firstResult);
+        System.out.println("Первичный результат: " + firstResult);
         System.out.println("Значение в кеше: " + cachedValue);
 
         assertNotNull(cachedValue, "Результат должен быть закэширован");
 
-        cache.evict(USER_ID + "_" + QUERY);
+        Boolean secondResult = recommendationRepository.checkProductRules(USER_ID_1, QUERY);
 
-        Boolean secondResult = recommendationRepository.checkProductRules(USER_ID, QUERY);
-        assertNotNull(secondResult, "Вторичный результат не должен быть null");
-
-        System.out.println("Второй результат: " + secondResult);
+        System.out.println("Вторичный результат: " + secondResult);
 
         assertEquals(firstResult, secondResult, "Результаты должны быть одинаковыми");
 
-        Cache.ValueWrapper secondCacheValue = cache.get(USER_ID + "_" + QUERY);
+        Cache.ValueWrapper secondCacheValue = cache.get(USER_ID_1 + "_" + QUERY);
         assertNotNull(secondCacheValue, "Результат должен быть извлечен из кеша");
         assertEquals(firstResult, secondCacheValue.get(), "Значение в кеше должно быть равно первичному результату");
     }
+
     @Test
+    @DirtiesContext
     void testCachePerformance() {
         long startTimeFirstRequest = System.currentTimeMillis();
-        Boolean firstResult = recommendationRepository.checkProductRules(USER_ID, QUERY);
+        Boolean firstResult = recommendationRepository.checkProductRules(USER_ID_2, QUERY);
         long endTimeFirstRequest = System.currentTimeMillis();
         long timeFirstRequest = endTimeFirstRequest - startTimeFirstRequest;
-        assertNotNull(firstResult, "Первоначальный результат не должен быть null");
 
         System.out.println("Время первого запроса (к БД): " + timeFirstRequest + " миллисекунд");
 
         long startTimeSecondRequest = System.currentTimeMillis();
-        Boolean secondResult = recommendationRepository.checkProductRules(USER_ID, QUERY);
+        Boolean secondResult = recommendationRepository.checkProductRules(USER_ID_2, QUERY);
         long endTimeSecondRequest = System.currentTimeMillis();
         long timeSecondRequest = endTimeSecondRequest - startTimeSecondRequest;
-        assertNotNull(secondResult, "Вторичный результат не должен быть null");
 
         System.out.println("Время второго запроса (из кеша): " + timeSecondRequest + " миллисекунд");
 
