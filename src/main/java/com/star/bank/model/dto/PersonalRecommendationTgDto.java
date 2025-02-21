018bd18d32429c4fb3c97ee2ae180b2d76f19915
Belousoveu <belousov.eu@gmail.com>
2025-02-21 11:26:49 +0300

    Added descriptions for API-Docs

--- src/main/java/com/star/bank/model/dto/PersonalRecommendationTgDto.java
+++ src/main/java/com/star/bank/model/dto/PersonalRecommendationTgDto.java
@@ -1,6 +1,7 @@
 package com.star.bank.model.dto;
 
 import com.star.bank.model.product.Product;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.List;
@@ -10,9 +11,16 @@ import java.util.List;
 @EqualsAndHashCode(exclude = "recommendations")
 @ToString
 @Builder
+@Schema(description = "Список рекомендаций пользователя с указанием Id пользователя")
 public class PersonalRecommendationTgDto {
+
+    @Schema(description = "Имя пользователя")
     private String firstName;
+
+    @Schema(description = "Фамилия пользователя")
     private String lastName;
+
+    @Schema(description = "Список рекомендаций пользователя")
     private List<Product> recommendations;
 
 }
