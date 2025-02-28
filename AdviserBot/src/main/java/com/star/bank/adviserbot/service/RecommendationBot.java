8e8cef0f6275eae545f1c9e8b53b1112659b4a3a
Belousoveu <belousov.eu@gmail.com>
2025-02-28 23:54:03 +0300

    fix: All bugs has been corrected

--- AdviserBot/src/main/java/com/star/bank/adviserbot/service/RecommendationBot.java
+++ AdviserBot/src/main/java/com/star/bank/adviserbot/service/RecommendationBot.java
@@ -5,24 +5,24 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import com.star.bank.model.dto.PersonalRecommendationTgDto;
-import com.star.bank.model.product.Product;
+import com.star.bank.adviserbot.dto.PersonalRecommendationTgDto;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
 public class RecommendationBot {
-    private static final Logger logger = LoggerFactory.getLogger(RecommendationBot.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecommendationBot.class);
+    private static final String RECOMMEND_COMMAND = "/recommend ";
 
     private TelegramBot bot;
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @Value("${telegram.bot.token}")
     private String token;
@@ -37,7 +37,7 @@ public class RecommendationBot {
     @PostConstruct
     public void init() {
         this.bot = new TelegramBot(token);
-        logger.info("Бот запущен");
+        LOGGER.info("Бот запущен");
         startBot();
     }
 
@@ -56,83 +56,76 @@ public class RecommendationBot {
         String text = update.message().text();
         long chatId = update.message().chat().id();
 
-        logger.info("Получено сообщение: {}", text);
+        LOGGER.info("Получено сообщение: {}", text);
 
         if (text.equals("/start")) {
             sendMessage(chatId, "Привет! Для получения рекомендаций по банковским продуктам, используйте команду /recommend Имя Фамилия!");
-        } else if (text.startsWith("/recommend ")) {
+        } else if (text.startsWith(RECOMMEND_COMMAND)) {
             handleRecommendCommand(chatId, text);
         }
     }
 
     private void handleRecommendCommand(long chatId, String text) {
-        String[] parts = text.split(" ", 3);
-        if (parts.length < 3 || !isValidName(parts[1]) || !isValidName(parts[2])) {
-            logger.warn("Неверный формат данных: {}", text);
+
+        if (!text.startsWith(RECOMMEND_COMMAND) || isInvalidName(text.substring(RECOMMEND_COMMAND.length()))) {
+            LOGGER.warn("Неверный формат данных: {}", text);
             sendMessage(chatId, "Неверный формат данных. Используйте команду в формате: /recommend Имя Фамилия");
             return;
         }
 
-        String firstName = parts[1];
-        String lastName = parts[2];
 
-        logger.info("Запрос на рекомендации для: {} {}", firstName, lastName);
+        String username = text.substring(RECOMMEND_COMMAND.length());
+        LOGGER.info("Запрос на рекомендации для: {}", username);
 
-        PersonalRecommendationTgDto response = getRecommendationsFromBank(firstName, lastName);
-        if (response != null) {
-            sendRecommendations(chatId, response);
-        } else {
-            sendMessage(chatId, "Сервис временно недоступен. Пожалуйста, попробуйте позже");
-        }
+        sendMessage(chatId, getRecommendationsFromBank(username));
     }
 
-    private PersonalRecommendationTgDto getRecommendationsFromBank(String firstName, String lastName) {
-        String url = String.format("%s/recommendation/username/%s", mainAppUrl, firstName);
+    private String getRecommendationsFromBank(String username) {
+        String url = String.format("%s/recommendation/username/%s", mainAppUrl, username);
 
-        logger.info("Запрос к сервису по URL: {}", url);
+        LOGGER.info("Запрос к сервису по URL: {}", url);
 
         try {
-            return restTemplate.getForObject(url, PersonalRecommendationTgDto.class);
-        } catch (RestClientException e) {
-            logger.error("Ошибка при обращении к сервису: {}", e.getMessage());
+            return sendRecommendations(restTemplate.getForObject(url, PersonalRecommendationTgDto.class));
+        } catch (RestClientException | NullPointerException e) {
+            LOGGER.error("Ошибка при обращении к сервису: {}", e.getMessage());
 
-            if (e.getCause() instanceof HttpClientErrorException.NotFound) {
-                logger.warn("Пользователь не найден: {}", firstName, lastName);
-                return null;
+            if (e instanceof HttpClientErrorException.NotFound) {
+                LOGGER.warn("Пользователь не найден: {}", username);
+                return String.format("Пользователь %s не найден", username);
             }
-            return null;
+            return "Ошибка при обращении к сервису. Пожалуйста, попробуйте позже.";
         }
     }
 
-    private void sendRecommendations(long chatId, PersonalRecommendationTgDto recommendation) {
+    private String sendRecommendations(PersonalRecommendationTgDto recommendation) {
 
-        logger.info("Отправка рекомендаций для пользователя: {} {}", recommendation.getFirstName(), recommendation.getLastName());
+        LOGGER.info("Отправка рекомендаций для пользователя: {} {}", recommendation.firstName(), recommendation.lastName());
 
         StringBuilder messageText = new StringBuilder();
-        messageText.append("Здравствуйте, ").append(recommendation.getFirstName()).append(" ").append(recommendation.getLastName()).append("\n\n");
-        messageText.append("Новые продукты для вас:\n");
+        messageText.append("Здравствуйте, ").append(recommendation.firstName()).append(" ").append(recommendation.lastName()).append("\n\n");
 
-        if (recommendation.getRecommendations().isEmpty()) {
+        if (recommendation.recommendations().isEmpty()) {
             messageText.append("Нет новых продуктов для вас.");
         } else {
-            for (Product product : recommendation.getRecommendations()) {
-                messageText.append("- ").append(product.getName()).append("\n");
-                messageText.append(product.getText()).append("\n\n");
+            messageText.append("Новые продукты для вас:\n");
+            for (PersonalRecommendationTgDto.Product product : recommendation.recommendations()) {
+                messageText.append("- ").append(product.name()).append("\n");
+                messageText.append(product.text()).append("\n\n");
             }
         }
-
-        sendMessage(chatId, messageText.toString());
+        return messageText.toString();
     }
 
     private void sendMessage(long chatId, String text) {
         SendMessage message = new SendMessage(chatId, text);
         SendResponse response = bot.execute(message);
         if (!response.isOk()) {
-            logger.error("Ошибка при отправке сообщения: {}", response.description());
+            LOGGER.error("Ошибка при отправке сообщения: {}", response.description());
         }
     }
 
-    private boolean isValidName(String name) {
-        return name.matches("[A-Za-zА-Яа-яЁё]+");
+    private boolean isInvalidName(String name) {
+        return !name.matches("^[a-z]+\\.[a-z]+$");
     }
 }
