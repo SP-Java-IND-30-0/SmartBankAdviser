eceebbac1cce51387945c919393fd005bd97534a
Belousoveu <belousov.eu@gmail.com>
2025-03-01 15:57:08 +0300

    test: An integration test has been created to verify the following endpoints: GET: /recommendation/{userId} GET: /recommendation/username/{username} GET: /rule/stats

--- src/test/java/com/star/bank/TestDynamicRule.java
+++ src/test/java/com/star/bank/TestDynamicRule.java
@@ -1,21 +1,19 @@
 package com.star.bank;
 
-import com.star.bank.model.enums.BankProductType;
-import com.star.bank.model.enums.CompareType;
-import com.star.bank.model.enums.OperationType;
+import com.star.bank.model.dto.DynamicRuleDto;
+import com.star.bank.model.dto.SimpleRuleDto;
 import com.star.bank.model.enums.QueryType;
-import com.star.bank.model.product.DynamicRule;
-import com.star.bank.model.rule.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 public class TestDynamicRule {
 
 
-    public static DynamicRule getAlwaysTrueProduct() {
-        DynamicRule rule = new DynamicRule();
+    public static DynamicRuleDto getAlwaysTrueProduct() {
+        DynamicRuleDto rule = new DynamicRuleDto();
         rule.setProductId(UUID.randomUUID());
         rule.setProductName("Product for all users");
         rule.setProductText("This products rules always return true");
@@ -23,46 +21,46 @@ public class TestDynamicRule {
         return rule;
     }
 
-    public static DynamicRule getAlwaysFalseProduct() {
-        DynamicRule rule = new DynamicRule();
+    public static DynamicRuleDto getAlwaysFalseProduct() {
+        DynamicRuleDto rule = new DynamicRuleDto();
         rule.setProductId(UUID.randomUUID());
         rule.setProductName("Product for no one");
         rule.setProductText("This products rules always return false");
 
-        SimpleRule rule1 = new SimpleRule();
+        SimpleRuleDto rule1 = new SimpleRuleDto();
         rule1.setQueryType(QueryType.TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW);
         rule1.setNegate(false);
-        rule1.setArguments(new RuleCompareOperationSum(BankProductType.DEBIT, CompareType.GT));
+        rule1.setArguments(List.of("DEBIT", ">"));
 
-        SimpleRule rule2 = new SimpleRule();
+        SimpleRuleDto rule2 = new SimpleRuleDto();
         rule2.setQueryType(QueryType.TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW);
         rule2.setNegate(false);
-        rule2.setArguments(new RuleCompareOperationSum(BankProductType.DEBIT, CompareType.LT));
+        rule2.setArguments(List.of("DEBIT", "<"));
 
         rule.setRules(Set.of(rule1, rule2));
         return rule;
     }
 
-    public static DynamicRule getTestProduct() {
-        DynamicRule rule = new DynamicRule();
+    public static DynamicRuleDto getTestProduct() {
+        DynamicRuleDto rule = new DynamicRuleDto();
         rule.setProductId(UUID.randomUUID());
         rule.setProductName("Test product");
         rule.setProductText("This products rules test");
 
-        SimpleRule rule1 = new SimpleRule();
+        SimpleRuleDto rule1 = new SimpleRuleDto();
         rule1.setQueryType(QueryType.ACTIVE_USER_OF);
         rule1.setNegate(true);
-        rule1.setArguments(new RuleUserOf(BankProductType.DEBIT));
+        rule1.setArguments(List.of("DEBIT"));
 
-        SimpleRule rule2 = new SimpleRule();
+        SimpleRuleDto rule2 = new SimpleRuleDto();
         rule2.setQueryType(QueryType.TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW);
         rule2.setNegate(false);
-        rule2.setArguments(new RuleCompareOperationSum(BankProductType.CREDIT, CompareType.GT));
+        rule2.setArguments(List.of("CREDIT", ">"));
 
-        SimpleRule rule3 = new SimpleRule();
+        SimpleRuleDto rule3 = new SimpleRuleDto();
         rule3.setQueryType(QueryType.TRANSACTION_SUM_COMPARE);
         rule3.setNegate(false);
-        rule3.setArguments(new RuleCompareSum(BankProductType.SAVING, OperationType.DEPOSIT, CompareType.LE, 1000));
+        rule3.setArguments(List.of("SAVING", "DEPOSIT", "<=", "1000"));
 
         rule.setRules(Set.of(rule1, rule2, rule3));
         return rule;
